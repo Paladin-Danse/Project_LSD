9c95ba47f77be2e06f7dcea4b2c5dc1a88332ffb
fmmscogh <154488249+fmmscogh@users.noreply.github.com>
2024-03-08 19:26:41 +0900

    <add> 스크립트 추가
    PlanetSelcet 스크립트 추가

--- Assets/Scripts/Map/DungeonEntranceUI.cs
+++ Assets/Scripts/Map/DungeonEntranceUI.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class DungeonEntranceUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static DungeonEntranceUI instance;
+    public GameObject GreenLine;
 
-    // Update is called once per frame
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    private void Start()
+    {
+        Cursor.visible = true;
+        GreenLine.SetActive(false);
+    }
     void Update()
     {
-        
+        transform.position = Input.mousePosition;
+        //if(EventSystem.current.IsPointerOverGameObject() == true)
+        //{
+        //    GreenLine.SetActive(true);
+        //}
+        //else if(EventSystem.current.IsPointerOverGameObject() == false)
+        //{
+        //    GreenLine.SetActive(false);
+        //}
+    }
+
+    public void OnGreenLine()
+    {
+        GreenLine.SetActive(true);
+    }
+
+    public void OffGreenLine()
+    {
+        GreenLine.SetActive(false);
     }
 }
