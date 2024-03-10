31d3f4aa3245ac8e4a38983026bb02479a978566
fmmscogh <154488249+fmmscogh@users.noreply.github.com>
2024-03-10 22:21:59 +0900

    <feature,add> 던전입장 창 뜨기 및 던전 데이터 추가
    행성 이미지 누르면 던전 입장 버튼 활성화
    던전 데이터 스크립트 추가

---
+++ Assets/Scripts/Map/DungeonSelect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DungeonSelect : MonoBehaviour
+{
+    public GameObject GreenLine;    
+    public GameObject dungeonEntrancePanel;
+    public TextMeshProUGUI dungeonNameOfPanel;
+    public DungeonData[] SelectedDungeon; 
+
+    public void MouseOverUI()
+    {
+        GreenLine.SetActive(true);
+    }
+
+    public void MouseExitUI()
+    {
+        GreenLine.SetActive(false);
+    }
+
+    public void OnDungeonEntrancePanel(int dungeonNum)
+    {
+        dungeonNameOfPanel.text = SelectedDungeon[dungeonNum].dungeonName;
+        dungeonEntrancePanel.SetActive(true);
+    }
+
+    public void OffDungeonEntrancePanel()
+    {
+        dungeonEntrancePanel.SetActive(false);
+    }
+}
