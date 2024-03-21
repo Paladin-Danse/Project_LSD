07c17405990c9a440b47e3b7850ccee401b6ac0c
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-03-22 04:44:56 +0900

    <feature> 이전작업폴더 삭제, NewWeaponInventory 작업폴더 생성

---
+++ Assets/NewWeaponInventory/Scripts/Items/DropItemObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData item;
+
+    public string GetInteractPrompt()
+    {
+        return string.Format("Pickup {0}", item.displayName);
+    }
+
+    public void OnInteract()
+    {
+        Inventory.instance.AddItem(item); // 인벤토리(싱클톤화)에 아이템 추가하기
+        Destroy(gameObject); // 아이템 제거
+    }
+}
--- Assets/NewWeaponInventory/Scripts/Items/DropWeaponObject.cs
+++ Assets/NewWeaponInventory/Scripts/Items/DropWeaponObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemEntranceObject : MonoBehaviour, IInteractable
+public class DropWeaponObject : MonoBehaviour, IInteractable
 {
     public ItemData item;
 
