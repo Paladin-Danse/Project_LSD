5fd024f19febf4e390e8edab05a3fe36b95e88d1
fmmscogh <154488249+fmmscogh@users.noreply.github.com>
2024-03-26 20:12:08 +0900

    <add> 사막 맵 보스유닛 에셋 추가

--- Assets/Scripts/Enemy/EnemyProjectile.cs
+++ Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -47,7 +47,7 @@ public class EnemyProjectile : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Health>().TakeDamageWithoutDefense(damage);
+            collision.gameObject.GetComponent<Health>().TakeDamage(damage);
             Debug.Log(damage);
             Debug.Log("Player Hit");
             Destroy(gameObject);
