f37a5c3b4fbce6c7e266c8838d23e54bde2d1b63
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-04-03 20:58:19 +0900

    <feature> 무기줍기 상호작용 일부구현
    불필요한 C#스크립트 파일 2개 삭제

--- Assets/NewInventoryInAmmo/WeaponSlotUI.cs
+++ Assets/NewInventoryInAmmo/WeaponSlotUI.cs
@@ -10,13 +10,13 @@ public class WeaponSlotUI : MonoBehaviour
     public Button button;
     public Image icon;
     public TextMeshProUGUI nameText;
-    private ItemSlot curWeapon;
+    private WeaponSlot curWeapon;
 
     public int index;
     public bool equipped;
 
     // 아이템 셋팅
-    public void Set(ItemSlot slot)
+    public void Set(WeaponSlot slot)
     {
         curWeapon = slot; // 슬롯에 들어간 무기
         icon.gameObject.SetActive(true); // 아이콘 표시
