24fec71c09d0c5535737fd4ebbd3b8a58a63ab2c
이승준 <37408775+Paladin-Danse@users.noreply.github.com>
2024-04-09 12:52:29 +0900

    <feature> UI가 실행되는 동안 플레이어의 조작을 막는 기능 추가.

--- Assets/Scripts/Player/PlayerInput.cs
+++ Assets/Scripts/Player/PlayerInput.cs
@@ -21,4 +21,31 @@ public class PlayerInput : MonoBehaviour
     {
         inputAction.Disable();
     }
+    public void SetCursorLock(bool setBool)
+    {
+        if (setBool)
+        {
+            playerActions.Jump.Enable();
+            playerActions.Shoot.Enable();
+            playerActions.Move.Enable();
+            playerActions.Reload.Enable();
+            playerActions.Aim.Enable();
+            playerActions.Look.Enable();
+            playerActions.WeaponSwap.Enable();
+
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            playerActions.Jump.Disable();
+            playerActions.Shoot.Disable();
+            playerActions.Move.Disable();
+            playerActions.Reload.Disable();
+            playerActions.Aim.Disable();
+            playerActions.Look.Disable();
+            playerActions.WeaponSwap.Disable();
+
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
 }
