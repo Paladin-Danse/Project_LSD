a80fc8cad60f9e1099990002d6739ef8cfe6a0c2
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-04-12 17:25:59 +0900

    <fix> 사운드 슬라이더 튀는 오류 수정

--- Assets/NewIntroScene/SettingSoundMenu.cs
+++ Assets/NewIntroScene/SettingSoundMenu.cs
@@ -15,9 +15,9 @@ public class SettingSoundMenu : MonoBehaviour
         SoundManager.instance.audioMixer.GetFloat("Master", out float m1);
         SoundManager.instance.audioMixer.GetFloat("SFX", out float m2);
         SoundManager.instance.audioMixer.GetFloat("BGM", out float m3);
-        masterAudioSlider.value = (m1 + 20) / 40;
-        SFXAudioSlider.value = (m2 + 20) / 40;
-        BGMAudioSlider.value = (m3 + 20) / 40;
+        masterAudioSlider.value = m1 > -80f ? (m1 + 20) / 40 : -80f;
+        SFXAudioSlider.value = m2 > -80f ? (m2 + 20) / 40 : -80f;
+        BGMAudioSlider.value = m3 > -80f ? (m3 + 20) / 40 : -80f;
         masterAudioSlider.onValueChanged.AddListener(val => OnSoundSliderValChanged("Master", val));
         SFXAudioSlider.onValueChanged.AddListener(val => OnSoundSliderValChanged("SFX", val));
         BGMAudioSlider.onValueChanged.AddListener(val => OnSoundSliderValChanged("BGM", val));
