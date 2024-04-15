27defa3b7af5d1ba4068b5492dc1cd22a14d64a8
fmmscogh <154488249+fmmscogh@users.noreply.github.com>
2024-04-15 11:01:23 +0900

    <feature> 인트로 스토리 텍스트와 사운드 추가

--- Assets/NewIntroScene/IntroSceneManager.cs
+++ Assets/NewIntroScene/IntroSceneManager.cs
@@ -9,9 +9,13 @@ public class IntroSceneManager : MonoBehaviour
     public GameObject skipButton;
     public GameObject nextButton;
     public GameObject MainMenuButtons;
+    AudioSource audioSource;
+    public AudioClip introStroySound;
+    public GameObject stotyBackGround;
     private void Awake()
     {
         animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -22,19 +26,23 @@ public class IntroSceneManager : MonoBehaviour
     public void StoryAnimatorStart()
     {
         animator.SetTrigger("Play");
+        audioSource.PlayOneShot(introStroySound);
         MainMenuButtons.SetActive(false);
         skipButton.SetActive(true);
+        stotyBackGround.SetActive(true);
     }
     public void StoryAnimatorEnd() 
     {
         Debug.Log("스토리 애니메이션 종료");
         nextButton.SetActive(true);
         skipButton.SetActive(false);
+        stotyBackGround.SetActive(false);
     }
 
     public void SkipButton()
     {
         animator.SetTrigger("Skip");
+        audioSource.Stop();
     }
 
     public void GameStart()
