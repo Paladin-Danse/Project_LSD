1e2838ab5cd5cc2b3fc6e492602c565a210f6113
이승준 <37408775+Paladin-Danse@users.noreply.github.com>
2024-04-16 22:17:27 +0900

    <feature> 새로운 HUD에 사용할 인벤토리 탄약 구하는 코드 구현.
    - 새로운 HUD에 인벤토리 탄약 구하는 코드를 PlayerWeaponUI 삽입

--- Assets/Scripts/Player/PlayerUI/PlayerWeaponUI.cs
+++ Assets/Scripts/Player/PlayerUI/PlayerWeaponUI.cs
@@ -42,5 +42,6 @@ public class PlayerWeaponUI : MonoBehaviour, IPlayerUIInterface
     {
         // todo : Inventory Ammo와 연결
         // 무기 종류에 따라 Ammo 연결 다르게 해줘야 함
+        playerInventoryAmmoText.text = $"{Player.Instance.inventory.InventoryAmmoCheck(playerCharacter.curWeapon_AmmoType)}";
     }
 }
