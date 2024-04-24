956d63071d053ef140a1e906f0a378ec362e174c
Siryu <siryu2409@gmail.com>
2024-04-24 16:36:48 +0900

    <Build> 빌드 설정 및 텍스쳐 퀄리티 조정

--- Assets/Scripts/Weapon/StateMachine/GunBaseState.cs
+++ Assets/Scripts/Weapon/StateMachine/GunBaseState.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using Unity.VisualScripting;
-using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
