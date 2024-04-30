2aded89ca37d8c4e893927a46387054c68dcf11e
이승준 <37408775+Paladin-Danse@users.noreply.github.com>
2024-04-30 14:53:47 +0900

    <fix> 이펙트 오브젝트 재활용하지 않는 문제 해결.

--- Assets/Scripts/Weapon/AmmoProjectile.cs
+++ Assets/Scripts/Weapon/AmmoProjectile.cs
@@ -83,7 +83,7 @@ public class AmmoProjectile : MonoBehaviour
         }
     }
     
-    private void DestroyProjectile() 
+    private void DestroyProjectile()
     {
         ObjectPoolManager.Instance.TryPush(this.gameObject);
     }
